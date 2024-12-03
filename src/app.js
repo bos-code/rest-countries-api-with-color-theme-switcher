a174141360c3e0b0f872af37841af0ad77b69a5d
Henry chidera <chidera9713@gmail.com>
2024-12-03 04:49:01 +0100

    search and region container markup

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
-const sunIcon  = document.querySelector('.sun')
-const moonIcon  = document.querySelector('.moon')
+const sunIcon = document.querySelector('.sun')
+const moonIcon = document.querySelector('.moon')
 const UserTheme = localStorage.getItem("them")
-const systemTheme  = window.matchMedia("[prefers-coloe-scheme: dark]").matches
+const systemTheme = window.matchMedia("[prefers-coloe-scheme: dark]").matches
+const drop = document.querySelector('.drop')
+let down = document.querySelector('.down')
+console.log(drop);
+
+
 
 const cardContainer = document.getElementById("root");
 console.log(cardContainer);
@@ -19,12 +24,13 @@ const cardMarkUp = `
     </li>`;
 
 const countries = [];
+const region = []
 
 async function getCountries() {
-    const response = await fetch("/data.json");
-    const data = await response.json();
+  const response = await fetch("/data.json");
+  const data = await response.json();
 
-   
+
 
   data.forEach((country) => {
     let temp = cardMarkUp.replaceAll(":NAME", country.name);
@@ -34,31 +40,37 @@ async function getCountries() {
     temp = temp.replaceAll(":CAPITAL", country.capital);
 
     countries.push(country);
-    
+    const reg = country.region
+
+    region.push(reg)
     // cardContainer.insertAdjacentHTML("beforeend", temp);
-});
+  });
+  const allRegions = [...new Set(region)]
+  return allRegions;
 }
 
 
 getCountries();
 
 
-const toggleIcon = function(){
+
+
+const toggleIcon = function () {
   moonIcon.classList.toggle("display-none")
   sunIcon.classList.toggle("display-none")
 }
 
-const themeCheck = function() {
+const themeCheck = function () {
   if (UserTheme === "dark" || (!UserTheme && systemTheme)) {
     document.documentElement.classList.add("dark")
     moonIcon.classList.add("display-none")
     return
   }
   sunIcon.classList.add("display-none")
-} 
+}
 
-const themeSwitch = function(){
-  if (document.documentElement.classList.contains("dark")){
+const themeSwitch = function () {
+  if (document.documentElement.classList.contains("dark")) {
     document.documentElement.classList.remove("dark")
     localStorage.setItem("theme", "light")
     toggleIcon();
@@ -74,3 +86,25 @@ moonIcon.addEventListener("click", themeSwitch)
 sunIcon.addEventListener("click", themeSwitch)
 
 themeCheck()
+
+const toggleDropdown = function () {
+  down.classList.toggle('display-none')
+}
+drop.addEventListener('click',
+  toggleDropdown
+)
+
+const regionTemp = ` <li class=" list-none">:REG</li>`
+
+const renderRegion = function () {
+  let down = ''
+  renderRegion.bind(getCountries.bind(this.allRegions))
+  console.log(allRegions);
+  allRegions.forEach(reg => {
+
+    const regionTemp = ` <li class=" list-none">${reg}</li>`
+    down.insertAdjacentHTML('beforebegin', regionTemp)
+  })
+
+}
+window.addEventListener('load', renderRegion)
