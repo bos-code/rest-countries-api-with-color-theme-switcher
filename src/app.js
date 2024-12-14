ec49f992a2983275e187ac8e5a2ff7a21c03226b
prosperemebo <emeboprosper@gmail.com>
2024-12-14 12:37:04 +0100

    chore: format javascript document

--- src/app.js
+++ src/app.js
@@ -1,196 +1,190 @@
-const sunIcon = document.querySelector('.sun')
-const moonIcon = document.querySelector('.moon')
-const UserTheme = localStorage.getItem("them")
-const systemTheme = window.matchMedia("[prefers-coloe-scheme: dark]").matches
-const drop = document.querySelector('.drop')
-let down = document.querySelector('.down')
-const navBtn = document.getElementById('home')
-const contentBox = document.querySelector('.modal')
-const searchBox = document.getElementById('default-search')
-const searchIcon = document.getElementById('searchIcon')
-const contentContainer = document.querySelector('.contentBox')
+const sunIcon = document.querySelector('.sun');
+const moonIcon = document.querySelector('.moon');
+const UserTheme = localStorage.getItem('them');
+const systemTheme = window.matchMedia('[prefers-coloe-scheme: dark]').matches;
+const drop = document.querySelector('.drop');
+let down = document.querySelector('.down');
+const navBtn = document.getElementById('home');
+const contentBox = document.querySelector('.modal');
+const searchBox = document.getElementById('default-search');
+const searchIcon = document.getElementById('searchIcon');
+const contentContainer = document.querySelector('.contentBox');
 
-const cardContainer = document.getElementById("cardCont");
+const cardContainer = document.getElementById('cardCont');
 const cardMarkUp = `
-  <li data-name = ":NAME" class="cards   transition ease-in-out delay-150 hover:-translate-y-3 hover:scale-130  overflow-hidden duratioy-200    shadow-2xl rounded bg-[--appbar] flex list-none  flex-col ">
-      <figure class="w-full h-1/2 flex-1">  <img class="  w-full h-full block object-cover object-center"  src=":IMG" , alt=":NAME"></figure>
-         
-         <div class="textbox h-1/2 px-4 py-4 flex-1">
-            <p class="mb-2 text-[--text]  font-bold">:NAME</p>
-            <ul>
-              <li>
-                <p class="text-[--text] font-medium">population: <span class="span text-gray-400">:population</span></p>
-              </li>
-              <li>
-                <p class="text-[--text] font-medium">Region: <span class="span text-gray-400">:REGION</span></p>
-              </li>
-              <li>
-                <p class="text-[--text] font-medium">capital: <span class="span text-gray-400">:CAPITAL</span></p>
-              </li>
-            </ul>
-            </li>
-            </div>
-            `;
+  <li
+  data-name=":NAME"
+  class="cards transition ease-in-out delay-150 hover:-translate-y-3 hover:scale-130 overflow-hidden duratioy-200 shadow-2xl rounded bg-[--appbar] flex list-none flex-col"
+>
+  <figure class="w-full h-1/2 flex-1">
+    <img
+      class="w-full h-full block object-cover object-center"
+      src=":IMG"
+      ,
+      alt=":NAME"
+      loading="lazy"
+    />
+  </figure>
 
+  <div class="textbox h-1/2 px-4 py-4 flex-1">
+    <p class="mb-2 text-[--text] font-bold">:NAME</p>
+    <ul>
+      <li>
+        <p class="text-[--text] font-medium">
+          population: <span class="span text-gray-400">:population</span>
+        </p>
+      </li>
+      <li>
+        <p class="text-[--text] font-medium">
+          Region: <span class="span text-gray-400">:REGION</span>
+        </p>
+      </li>
+      <li>
+        <p class="text-[--text] font-medium">
+          capital: <span class="span text-gray-400">:CAPITAL</span>
+        </p>
+      </li>
+    </ul>
+  </div>
+</li>
+`;
 
 const countries = [];
-let region = []
+let region = [];
 
 async function getCountries() {
-  cardContainer.innerHTML = " "
+  cardContainer.innerHTML = ' ';
 
-  const response = await fetch("/data.json");
+  const response = await fetch('/data.json');
   const data = await response.json();
   console.log(data);
 
-let countNames = []
-
-
+  let countNames = [];
 
   data.map((country, i) => {
-    let temp = cardMarkUp.replaceAll(":NAME", country.name);
-    temp = temp.replaceAll(":IMG", country.flag)
-    temp = temp.replaceAll(":population", country.population);
-    temp = temp.replaceAll(":REGION", country.region);
-    temp = temp.replaceAll(":CAPITAL", country.capital);
-countNames.push({name: country.name})
+    let temp = cardMarkUp.replaceAll(':NAME', country.name);
+    temp = temp.replaceAll(':IMG', country.flag);
+    temp = temp.replaceAll(':population', country.population);
+    temp = temp.replaceAll(':REGION', country.region);
+    temp = temp.replaceAll(':CAPITAL', country.capital);
+    countNames.push({ name: country.name });
     countries.push(country);
-    
-    const reg = country.region
-    region.push(reg)
+
+    const reg = country.region;
+    region.push(reg);
 
     // cardContainer.insertAdjacentHTML("beforeend", temp);
 
     // if (reg == 'Americas') {
-    cardContainer.insertAdjacentHTML("beforeend", temp);
+    cardContainer.insertAdjacentHTML('beforeend', temp);
     // }
-// 
+    //
 
-    down.addEventListener("click", (e) => {
-      const regionList = e.target.closest(".regionList");
+    down.addEventListener('click', (e) => {
+      const regionList = e.target.closest('.regionList');
       if (regionList) {
         const filterRegion = regionList.dataset.region;
         if (reg == filterRegion) {
-          cardContainer.insertAdjacentHTML("afterbegin", temp);
+          cardContainer.insertAdjacentHTML('afterbegin', temp);
         }
-
       }
-    })
+    });
     // return
 
-
-
-
-
-    const allRegions = [...new Set(region)]
-    down.innerHTML = ''
-    allRegions.forEach(reg => {
+    const allRegions = [...new Set(region)];
+    down.innerHTML = '';
+    allRegions.forEach((reg) => {
       // console.log(reg);
-      const regionTemp = ` <li class="regionList list-none" data-region="${reg}">${reg}</li>`
-      down.insertAdjacentHTML('afterbegin', regionTemp)
-
-    })
-  
-  })
-
+      const regionTemp = ` <li class="regionList list-none" data-region="${reg}">${reg}</li>`;
+      down.insertAdjacentHTML('afterbegin', regionTemp);
+    });
+  });
 
   const searchFunctionality = function (evt) {
     // evt.preventDefault();
-    searchBox.addEventListener('click', (e) =>{
-      e.preventDefault()
-       console.log('come')
-    }
-      
-    )
-    if (searchBox.value != " " || searchBox.value <= 30) {
+    searchBox.addEventListener('click', (e) => {
+      e.preventDefault();
+      console.log('come');
+    });
+    if (searchBox.value != ' ' || searchBox.value <= 30) {
       // e.prevent
       searchBox.addEventListener('input', (e) => {
-        e.preventDefault()
-        countries.filter(country => {
-          let temp = cardMarkUp.replaceAll(":NAME", country.name);
-          temp = temp.replaceAll(":IMG", country.flag)
-          temp = temp.replaceAll(":population", country.population);
-          temp = temp.replaceAll(":REGION", country.region);
-          temp = temp.replaceAll(":CAPITAL", country.capital);
-          const cName = (country.name).toLowerCase()
-          const value = (searchBox.value).toLowerCase()
-          
-          const  letter = cName.split('')
+        e.preventDefault();
+        countries.filter((country) => {
+          let temp = cardMarkUp.replaceAll(':NAME', country.name);
+          temp = temp.replaceAll(':IMG', country.flag);
+          temp = temp.replaceAll(':population', country.population);
+          temp = temp.replaceAll(':REGION', country.region);
+          temp = temp.replaceAll(':CAPITAL', country.capital);
+          const cName = country.name.toLowerCase();
+          const value = searchBox.value.toLowerCase();
+
+          const letter = cName.split('');
           if (cName.includes(value)) {
             // cardContainer.innerHTML = ''
-            cardContainer.insertAdjacentHTML("beforeend", temp);
-            if(value == ''){
-              countries.map(coun => {
-
-                cardContainer.insertAdjacentHTML("beforeend", temp);
-              })
-
+            cardContainer.insertAdjacentHTML('beforeend', temp);
+            if (value == '') {
+              countries.map((coun) => {
+                cardContainer.insertAdjacentHTML('beforeend', temp);
+              });
             }
           }
           // console.log(letter);
-          if (value == cName ) {
-            cardContainer.innerHTML = ''
-            cardContainer.insertAdjacentHTML("beforeend", temp);
-         
+          if (value == cName) {
+            cardContainer.innerHTML = '';
+            cardContainer.insertAdjacentHTML('beforeend', temp);
           }
-        })
-      })
+        });
+      });
     }
-  }
-  searchFunctionality()
-console.log(countNames);
-  const many = []
+  };
+
+  searchFunctionality();
+  console.log(countNames);
+  const many = [];
   const moreModal = function () {
-
     cardContainer?.addEventListener('click', function (e) {
-      const cad = e.target.closest(".cards")
+      const cad = e.target.closest('.cards');
       if (cad) {
-
-        cardContainer.classList.add('hidden')
-        document.getElementById('root').classList.add('hidden')
-        document.querySelector('.modal').classList.remove('hidden')
-        const countryName = cad.dataset.name
-        data.filter((country) => {
-          if (country.name == countryName) {
-            console.log(country);  return country
-          }
-        }).map((country, i) => {
-          const name = country.name
-          const flag = country.flag
-          const population = country.population
-          const region = country.region
-          const subregion = country.subregion
-          const capital = country.capital
-          const currencies = [...country.currencies]
-
-
-
-
-          const [languages] = country.languages
-          const domain = country.topLevelDomain
-          const native = country.nativeName
-          const border = country.borders
-
-            ;
-          //   currencies.forEach( coun =>{
-          // console.log(coun.name);
-
-
-          //   })
-          // console.log(currencies);
-          border.map(coun => {
-            countries.filter(cub => {
-              if (cub.alpha3Code
-                == coun) {
-
-                console.log([cub.name]);
-              }
+        cardContainer.classList.add('hidden');
+        document.getElementById('root').classList.add('hidden');
+        document.querySelector('.modal').classList.remove('hidden');
+        const countryName = cad.dataset.name;
+        data
+          .filter((country) => {
+            if (country.name == countryName) {
+              console.log(country);
+              return country;
             }
-            )
-
           })
+          .map((country, i) => {
+            const name = country.name;
+            const flag = country.flag;
+            const population = country.population;
+            const region = country.region;
+            const subregion = country.subregion;
+            const capital = country.capital;
+            const currencies = [...country.currencies];
 
-          const markup = `
+            const [languages] = country.languages;
+            const domain = country.topLevelDomain;
+            const native = country.nativeName;
+            const border = country.borders;
+
+            //   currencies.forEach( coun =>{
+            // console.log(coun.name);
+
+            //   })
+            // console.log(currencies);
+            border.map((coun) => {
+              countries.filter((cub) => {
+                if (cub.alpha3Code == coun) {
+                  console.log([cub.name]);
+                }
+              });
+            });
+
+            const markup = `
           <div class="content justify-center flex items-center gap-32">
   <figure class=" flex-1"><img class="w-full" src="${flag}" alt="${flag}"></figure>
 
@@ -211,86 +205,70 @@ console.log(countNames);
     <p class=" mt-10 text-gray-400 flex gap-2 flex-wrap whitespace-nowrap"><span class=" text-gray-200">border countries:</span></p>
 
   </div>
-</div>`
+</div>`;
 
+            contentContainer.insertAdjacentHTML('beforeend', markup);
 
-          contentContainer.insertAdjacentHTML('beforeend', markup)
-
-
-          console.log(country, 1, many);
-        })
+            console.log(country, 1, many);
+          });
       }
-    })
-
-
-
-  }
-  moreModal()
-
-
+    });
+  };
+  moreModal();
 }
 navBtn.addEventListener('click', () => {
   console.log('come');
-  contentContainer.innerHTML = ' '
-})
+  contentContainer.innerHTML = ' ';
+});
 getCountries();
 
 const toggleDisplay = function () {
   if (navBtn) {
-    cardContainer.classList.remove('hidden')
-    document.getElementById('root').classList.remove('hidden')
+    cardContainer.classList.remove('hidden');
+    document.getElementById('root').classList.remove('hidden');
   }
-}
-navBtn.addEventListener('click', toggleDisplay)
-
+};
+navBtn.addEventListener('click', toggleDisplay);
 
 const toggleDropdown = function () {
-  down.classList.toggle('display-none')
-}
-down.addEventListener("click", (e) => {
+  down.classList.toggle('display-none');
+};
+down.addEventListener('click', (e) => {
   // down.classList.toggle('hidden')
-  toggleDropdown()
-  cardContainer.innerHTML = " "
-})
-getCountries.bind(region)
-
-
-
-
-
+  toggleDropdown();
+  cardContainer.innerHTML = ' ';
+});
+getCountries.bind(region);
 
 const toggleIcon = function () {
-  moonIcon.classList.toggle("display-none")
-  sunIcon.classList.toggle("display-none")
-}
+  moonIcon.classList.toggle('display-none');
+  sunIcon.classList.toggle('display-none');
+};
 
 const themeCheck = function () {
-  if (UserTheme === "dark" || (!UserTheme && systemTheme)) {
-    document.documentElement.classList.add("dark")
-    moonIcon.classList.add("display-none")
-    return
+  if (UserTheme === 'dark' || (!UserTheme && systemTheme)) {
+    document.documentElement.classList.add('dark');
+    moonIcon.classList.add('display-none');
+    return;
   }
-  sunIcon.classList.add("display-none")
-}
+  sunIcon.classList.add('display-none');
+};
 
 const themeSwitch = function () {
-  if (document.documentElement.classList.contains("dark")) {
-    document.documentElement.classList.remove("dark")
-    localStorage.setItem("theme", "light")
+  if (document.documentElement.classList.contains('dark')) {
+    document.documentElement.classList.remove('dark');
+    localStorage.setItem('theme', 'light');
     toggleIcon();
-    return
+    return;
   }
 
-  document.documentElement.classList.add("dark");
-  localStorage.setItem("theme", "light")
-  toggleIcon()
+  document.documentElement.classList.add('dark');
+  localStorage.setItem('theme', 'light');
+  toggleIcon();
+};
+moonIcon.addEventListener('click', themeSwitch);
+sunIcon.addEventListener('click', themeSwitch);
 
-}
-moonIcon.addEventListener("click", themeSwitch)
-sunIcon.addEventListener("click", themeSwitch)
+themeCheck();
 
-themeCheck()
-
-drop.addEventListener('click',
-  toggleDropdown
-)
+drop.addEventListener('click', toggleDropdown);
